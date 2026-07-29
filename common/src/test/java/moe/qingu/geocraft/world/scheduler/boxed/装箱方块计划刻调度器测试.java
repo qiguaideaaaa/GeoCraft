8653339d94a2732f1c4690eab88da8bbc1a324fc
qiguaideaaaa <66456117+qiguaideaaaa@users.noreply.github.com>
2026-07-29 18:49:49 +0800

    装箱测试忘记用扩展模拟区块世界了，补上

--- common/src/test/java/moe/qingu/geocraft/world/scheduler/boxed/装箱方块计划刻调度器测试.java
+++ common/src/test/java/moe/qingu/geocraft/world/scheduler/boxed/装箱方块计划刻调度器测试.java
@@ -32,7 +32,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import 清汩萌.天圆地方.util.网格工具;
 import 清汩萌.天圆地方.世界.扩展模拟区块世界;
-import 清汩萌.天圆地方.世界.模拟区块世界;
 
 import javax.annotation.Nonnull;
 import java.lang.reflect.InvocationTargetException;
@@ -66,7 +65,7 @@ public final class 装箱方块计划刻调度器测试 extends 方块计划刻�
     @SuppressWarnings("unused")
     public static void 测试全序方块调度_Inner(final @Nonnull Object[] $打包网格数据,final @Nonnull String $未解析的计划刻数据,final long[][] $测试时段){
         测试方块调度核心($打包网格数据,$未解析的计划刻数据, $测试时段,
-                i -> 模拟区块世界.构建(i,false),
+                i -> 扩展模拟区块世界.构建(i,false),
                 TotalOrderBoxedBlockTickScheduler::new,
                 BoxedBlockTickDatum::new);
     }
